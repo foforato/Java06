39eaf2fa65a9712888f066bb26029355af7494bf
foforato <47902396+foforato@users.noreply.github.com>
2019-10-01 19:36:36 +0200

    Update Classroom.java

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,9 +1,9 @@
 public class Classroom {
 
 	public static void main(String[] args) {
-		Wilder Amani = new Wilder("Amani",false);
+		Wilder lola = new Wilder("lola",false);
 		Wilder Aymane = new Wilder("Aymane",true);
-        System.out.println(Amani.whoAmI());
+        System.out.println(lola.whoAmI());
         System.out.println(Aymane.whoAmI());
 	}
 }
